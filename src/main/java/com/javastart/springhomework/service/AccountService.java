6753d2b7fc4560029a74aed33dd5afadfa7b7938
Alexey <groznovaa@gmail.comls>
2021-07-21 13:36:00 +0500

    Create AccountService

---
+++ src/main/java/com/javastart/springhomework/service/AccountService.java
@@ -0,0 +1,16 @@
+package com.javastart.springhomework.service;
+
+import com.javastart.springhomework.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+}
