7d493da990f4dddf54ce901eb046528ca5e35fed
Alexey <groznovaa@gmail.comls>
2021-07-21 13:40:39 +0500

    Create method sava in AccountService class

--- src/main/java/com/javastart/springhomework/service/AccountService.java
+++ src/main/java/com/javastart/springhomework/service/AccountService.java
@@ -1,8 +1,12 @@
 package com.javastart.springhomework.service;
 
+import com.javastart.springhomework.entity.Account;
+import com.javastart.springhomework.entity.Bill;
 import com.javastart.springhomework.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountService {
@@ -12,5 +16,10 @@ public class AccountService {
     @Autowired
     public AccountService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
+    }
+
+    public Long save(String name, String email, List<Bill> bills){
+        Account account = new Account(name, email, bills);
+        return accountRepository.save(account).getAccountId();
     }
 }
