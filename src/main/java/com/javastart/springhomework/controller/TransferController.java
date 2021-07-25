cb6991932d3a0ecfa43865b00971a1784e8a9ac7
Alexey <groznovaa@gmail.comls>
2021-07-25 13:22:20 +0500

    add object in transfer method

--- src/main/java/com/javastart/springhomework/controller/TransferController.java
+++ src/main/java/com/javastart/springhomework/controller/TransferController.java
@@ -1,13 +1,15 @@
 package com.javastart.springhomework.controller;
 
+import com.javastart.springhomework.controller.dto.TransferRequestDTO;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TransferController {
 
     @PostMapping("/transfers")
-    public Object transfer(){
+    public Object transfer(@RequestBody TransferRequestDTO transferRequestDTO){
 
     }
 }
--- src/main/java/com/javastart/springhomework/controller/dto/TransferRequestDTO.java
+++ src/main/java/com/javastart/springhomework/controller/dto/TransferRequestDTO.java
@@ -14,4 +14,28 @@ public class TransferRequestDTO {
 
     @JsonProperty("amount")
     private BigDecimal amount;
+
+    public Long getAccountIdFrom() {
+        return accountIdFrom;
+    }
+
+    public void setAccountIdFrom(Long accountIdFrom) {
+        this.accountIdFrom = accountIdFrom;
+    }
+
+    public Long getAccountIdTo() {
+        return accountIdTo;
+    }
+
+    public void setAccountIdTo(Long accountIdTo) {
+        this.accountIdTo = accountIdTo;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }
