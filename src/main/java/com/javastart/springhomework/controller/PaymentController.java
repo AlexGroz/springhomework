a499d7bc748c8df88b31673795f6581b63c9174b
Alexey <groznovaa@gmail.comls>
2021-07-25 17:58:33 +0500

    add variable paymentService, construcor for PaymentController, method pay in the class PaymentController

--- src/main/java/com/javastart/springhomework/controller/PaymentController.java
+++ src/main/java/com/javastart/springhomework/controller/PaymentController.java
@@ -1,7 +1,23 @@
 package com.javastart.springhomework.controller;
 
+import com.javastart.springhomework.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/payments")
+    public Object pay(@RequestBody){
+
+    }
 }
