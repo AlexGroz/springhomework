9cc3f6361f185a64786db764195515b6a6238c2b
Alexey <groznovaa@gmail.comls>
2021-07-25 13:25:50 +0500

    create TransferService

---
+++ src/main/java/com/javastart/springhomework/service/TransferService.java
@@ -0,0 +1,12 @@
+package com.javastart.springhomework.service;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class TransferService {
+    public Object transfer(Long accountIdFrom, Long accountIdTo, BigDecimal amount) {
+
+    }
+}
