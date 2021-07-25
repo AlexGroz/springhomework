f52eca4a9543372f647896b1e7152bbbdd8a8f20
Alexey <groznovaa@gmail.comls>
2021-07-25 13:19:58 +0500

    add annotation for variables in the TransferRequestDTO

--- src/main/java/com/javastart/springhomework/controller/dto/TransferRequestDTO.java
+++ src/main/java/com/javastart/springhomework/controller/dto/TransferRequestDTO.java
@@ -1,12 +1,17 @@
 package com.javastart.springhomework.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 
 public class TransferRequestDTO {
 
+    @JsonProperty("account_id_from")
     public Long accountIdFrom;
 
+    @JsonProperty("account_id_to")
     private Long accountIdTo;
 
+    @JsonProperty("amount")
     private BigDecimal amount;
 }
