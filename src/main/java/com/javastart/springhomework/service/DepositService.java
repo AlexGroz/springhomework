fb8d29d77bbd78a1938de40483cd419ca56b789f
Alexey <groznovaa@gmail.comls>
2021-08-02 19:22:41 +0500

    add DepositService

---
+++ src/main/java/com/javastart/springhomework/service/DepositService.java
@@ -0,0 +1,27 @@
+package com.javastart.springhomework.service;
+
+import com.javastart.springhomework.entity.Account;
+import com.javastart.springhomework.entity.Bill;
+import com.javastart.springhomework.utils.AccountUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class DepositService {
+    private final AccountService accountService;
+
+    @Autowired
+    public DepositService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public Object deposit(Long accountId, BigDecimal depositAmount){
+        Account account = accountService.getById(accountId);
+        Bill defaultBill = AccountUtils.findDefaultBill(account);
+        defaultBill.setAmount(defaultBill.getAmount().add(depositAmount));
+        accountService.update(account);
+        return "Success";
+    }
+}
