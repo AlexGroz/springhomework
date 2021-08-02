5c61c46f5593d5f39fb33d3ba30d8a49bb1ac774
Alexey <groznovaa@gmail.comls>
2021-08-02 19:25:20 +0500

    add DepositController

---
+++ src/main/java/com/javastart/springhomework/controller/DepositController.java
@@ -0,0 +1,19 @@
+package com.javastart.springhomework.controller;
+
+import com.javastart.springhomework.service.DepositService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DepositController {
+
+    private final DepositService depositService;
+
+    @Autowired
+    public DepositController(DepositService depositService) {
+        this.depositService = depositService;
+    }
+
+    public Object deposit(@RequestBody )
+}
