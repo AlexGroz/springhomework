b2aec0c35285d7ad0fa1b003186954449e45428b
Alexey <groznovaa@gmail.comls>
2021-08-02 19:31:27 +0500

    add Postmapping method deposit in the DepositController

--- src/main/java/com/javastart/springhomework/controller/DepositController.java
+++ src/main/java/com/javastart/springhomework/controller/DepositController.java
@@ -1,7 +1,9 @@
 package com.javastart.springhomework.controller;
 
+import com.javastart.springhomework.controller.dto.DepositRequestDTO;
 import com.javastart.springhomework.service.DepositService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +17,8 @@ public class DepositController {
         this.depositService = depositService;
     }
 
-    public Object deposit(@RequestBody )
+    @PostMapping("/deposit")
+    public Object deposit(@RequestBody DepositRequestDTO depositRequestDTO){
+        return depositService.deposit(depositRequestDTO.getAccountId(), depositRequestDTO.getAmount());
+    }
 }
